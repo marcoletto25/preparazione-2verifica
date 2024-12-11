96eaeff50421e4d41c9c6b653183a380e1d56592
marcoletto25 <bottari.marco@iisgalvanimi.edu.it>
2024-12-11 13:41:57 +0000

    punto 3

---
+++ preparazione-2verifica/src/app/macchina/macchina.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-macchina',
+  imports: [],
+  templateUrl: './macchina.component.html',
+  styleUrl: './macchina.component.css'
+})
+export class MacchinaComponent {
+
+}
